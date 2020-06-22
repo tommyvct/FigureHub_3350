78d8dcead5a16c6f54a3b5d2e27ca059d7266e80
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2020-06-21 19:00:01 -0500

    Create android studio project

---
+++ PBBS/app/src/main/java/comp3350/pbbs/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package comp3350.pbbs.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import comp3350.pbbs.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
