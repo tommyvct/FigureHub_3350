4106d201252b0fc292e4c2c2d44ddaa492e90644
AzizulHakim96 <56322456+AzizulHakim96@users.noreply.github.com>
2020-06-26 17:23:20 -0600

    created budgetCategories.java

--- app/src/main/java/comp3350/pbbs/CreditCard.java
+++
@@ -1,6 +0,0 @@
-package comp3350.pbbs;
-
-public class CreditCard
-{
-
-}
--- app/src/main/java/comp3350/pbbs/objects/budgetCategories.java
+++ app/src/main/java/comp3350/pbbs/objects/budgetCategories.java
@@ -1,5 +1,38 @@
 package comp3350.pbbs.objects;
 
 public class budgetCategories {
+    private String budgetID;
+    private double budgetLimit;
+    private String budgetName;
 
-}
+    public budgetCategories(String newID, double newLimit, String newBudgetName){
+        this.budgetID = newID;
+        this.budgetLimit = newLimit;
+        this.budgetName = newBudgetName;
+    }
+    public String getBudgetID(){
+        return budgetID;
+    }
+    public double getBudgetLimit(){
+        return budgetLimit;
+    }
+    public String getBudgetName(){
+        return budgetName;
+    }
+    public String toString(){
+        return "Budget ID: "+budgetID+" Budget Name: "+budgetName+" Budget Limit: "+budgetLimit;
+    }
+    public boolean equals(Object budgetObj){
+        boolean equal = false;
+        budgetCategories b;
+
+        if(budgetObj instanceof budgetCategories){
+            b = (budgetCategories) budgetObj;
+            if(((b.budgetID == null) && (budgetID == null))||(b.budgetID.equals((budgetID)))){
+                equal = true;
+            }
+        }
+        return equal;
+        }
+    }
+
