c2f1f325c13b98410221a5b2a3b343d65bd68e6c
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2020-06-28 02:45:02 -0500

    Further work on Transaction & tests

--- app/src/main/java/comp3350/pbbs/objects/Transaction.java
+++ app/src/main/java/comp3350/pbbs/objects/Transaction.java
@@ -1,44 +1,61 @@
 package comp3350.pbbs.objects;
 
 
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 
 public class Transaction {
     private int transactionID;
     private static int nextID = 1;
     private LocalDateTime time;
+    private float amount;
     // TODO: Make these fields correspond to the objects rather than id.
     private int cardID;
     private int budgetCategoryID;
-    public Transaction(LocalDateTime transactionTime, int card, int budgetCategory) {
+    public Transaction(LocalDateTime transactionTime, float amount, int card, int budgetCategory) {
         if(transactionTime == null)
             throw new IllegalArgumentException("A Transaction needs a time.");
+        if(amount < 0)
+            throw new IllegalArgumentException("Expected a positive transaction amount.");
         if(card <= 0)
             throw new IllegalArgumentException("Expected a positive card id.");
         if(budgetCategory <= 0)
             throw new IllegalArgumentException("Expected a positive budget category id.");
         time = transactionTime;
+        this.amount = amount;
         cardID = card;
+        budgetCategoryID = budgetCategory;
         transactionID = nextID;
         nextID++;
     }
 
     public LocalDateTime getTime() { return time; }
+    public float getAmount() { return amount; }
     public int getCard() { return cardID; }
     public int getBudgetCategory() { return budgetCategoryID; }
 
-    public boolean equals(Transaction other) {
-        boolean toReturn;
-        if(other == null) {
-            toReturn = false;
-        }
-        else {
+    public boolean equals(Object other) {
+        boolean toReturn = false;
+        if(other != null && other instanceof Transaction) {
+            Transaction otherTransaction = (Transaction) other;
             // TODO: Change these to .equals
-            boolean budgetsEqual = this.getBudgetCategory() == (other.getBudgetCategory());
-            boolean cardEqual = this.getCard() == (other.getCard());
-            boolean timeEqual = this.getTime().equals(other.getTime());
-            toReturn = budgetsEqual && cardEqual && timeEqual;
+            boolean budgetsEqual = this.getBudgetCategory() == (otherTransaction.getBudgetCategory());
+            DecimalFormat rounding = new DecimalFormat("0.00");
+            boolean amountEqual = rounding.format(this.getAmount()).equals(rounding.format(otherTransaction.getAmount()));
+            boolean cardEqual = this.getCard() == (otherTransaction.getCard());
+            boolean timeEqual = this.getTime().equals(otherTransaction.getTime());
+            toReturn = budgetsEqual && amountEqual && cardEqual && timeEqual;
         }
         return toReturn;
+    }
+
+    public String toString()
+    {
+        return "Transaction: " +
+                "ID: " + transactionID +
+                " Amount: " + amount +
+                " Time: " + time +
+                " Card: " + cardID +
+                " Budget Category: " + budgetCategoryID;
     }
 }
--- app/src/test/java/comp3350/pbbs/objects/TestTransaction.java
+++ app/src/test/java/comp3350/pbbs/objects/TestTransaction.java
@@ -7,36 +7,76 @@ import java.time.LocalDateTime;
 import comp3350.pbbs.objects.Transaction;
 import java.time.LocalDateTime;
 
+import static org.junit.Assert.assertNotEquals;
 
-public class TestTransaction extends TestCase
-{
-    public void testInvalidTransactions() {
-        try {
-            Transaction transaction = new Transaction(null, 1, 1);
-            fail("Expected IllegalArgumentException");
-        }
-        catch (IllegalArgumentException iae) { }
 
-        try {
-            Transaction transaction = new Transaction(LocalDateTime.now(), -2, 1);
-            fail("Expected IllegalArgumentException");
-        }
-        catch (IllegalArgumentException iae) { }
+public class TestTransaction extends TestCase {
+    private LocalDateTime now;
+    private int card;
+    private int budgetCategory;
+    private float amount;
 
+    public void setUp() {
+        now = LocalDateTime.now();
+        card = 1;
+        budgetCategory = 1;
+        amount = 5.57f;
+    }
+
+    public void testTransactionDates() {
+
+        Transaction transaction = new Transaction(now, amount, card, budgetCategory);
+        Transaction otherTransaction = new Transaction(now, amount, card, budgetCategory);
+        assertEquals(transaction, otherTransaction);
+        otherTransaction = new Transaction(now.plusDays(21), amount, card, budgetCategory);
+        assertNotEquals(transaction, otherTransaction);
         try {
-            Transaction transaction = new Transaction(LocalDateTime.now(), 1, -2);
+            new Transaction(null, amount, card, budgetCategory);
             fail("Expected IllegalArgumentException");
         }
         catch (IllegalArgumentException iae) { }
     }
 
-    public void testGetters() {
-        LocalDateTime now = LocalDateTime.now();
-        int card = 1;
-        int budgetCategory = 1;
-        Transaction transaction = new Transaction(now, card, budgetCategory);
-        assertEquals(transaction.getTime(), now);
-        assertEquals(transaction.getCard(), card);
-        assertEquals(transaction.getBudgetCategory(), budgetCategory);
+    public void testTransactionAmounts() {
+        Transaction transaction = new Transaction(now, amount, card, budgetCategory);
+        Transaction otherTransaction = new Transaction(now, amount, card, budgetCategory);
+        assertEquals(transaction, otherTransaction);
+        otherTransaction = new Transaction(now, amount + 1, card, budgetCategory);
+        assertNotEquals(transaction, otherTransaction);
+        try {
+            new Transaction(now, -0.24f, card, budgetCategory);
+            fail("Expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae) { }
+    }
+
+    public void testInvalidCards() {
+        // TODO: Change this to work with the CreditCard object
+        Transaction transaction = new Transaction(now, amount, card, budgetCategory);
+        Transaction otherTransaction = new Transaction(now, amount, card, budgetCategory);
+        assertEquals(transaction, otherTransaction);
+        int otherCard = card + 1;
+        otherTransaction = new Transaction(now, amount, otherCard, budgetCategory);
+        assertNotEquals(transaction, otherTransaction);
+        try {
+            new Transaction(now, amount, -1, budgetCategory);
+            fail("Expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae) { }
+    }
+
+    public void testInvalidBudgetCategories() {
+        // TODO: Change this to work with the BudgetCategory object
+        Transaction transaction = new Transaction(now, amount, card, budgetCategory);
+        Transaction otherTransaction = new Transaction(now, amount, card, budgetCategory);
+        assertEquals(transaction, otherTransaction);
+        int otherBudgetCategory = budgetCategory + 1;
+        otherTransaction = new Transaction(now, amount, card, otherBudgetCategory);
+        assertNotEquals(transaction, otherTransaction);
+        try {
+            new Transaction(now, amount, card, -1);
+            fail("Expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae) { }
     }
 }
