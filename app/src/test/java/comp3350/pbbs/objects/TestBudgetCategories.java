b355edd1dff4f97ace1c9239926b5d1b437f7086
AzizulHakim96 <56322456+AzizulHakim96@users.noreply.github.com>
2020-06-28 23:09:09 -0600

    updated budgetCategories.java and TestBudgetCategories.java

--- app/src/test/java/comp3350/pbbs/objects/TestBudgetCategories.java
+++ app/src/test/java/comp3350/pbbs/objects/TestBudgetCategories.java
@@ -6,13 +6,23 @@ import comp3350.pbbs.objects.budgetCategories;
 
 import static org.junit.Assert.assertNotEquals;
 
+/**
+ * TestBudgetCategories
+ * Azizul Hakim
+ * PBBS
+ *
+ * This class defines a test suite for the budgetCategories class.
+ */
 public class TestBudgetCategories extends TestCase
 {
-    private String budgetID;
-    private String budgetName;
-    private Double budgetLimit;
-    private budgetCategories newBudget;
+    private String budgetID;                //Test budget id
+    private String budgetName;              //Test budget name
+    private Double budgetLimit;             //Test budget limit
+    private budgetCategories newBudget;     //Test budgetCategories object
 
+    /**
+     * Method to set the test values.
+     */
     public void setUp(){
       budgetID = "1";
       budgetName = "Groceries";
@@ -20,23 +30,41 @@ public class TestBudgetCategories extends TestCase
       newBudget = new budgetCategories(budgetID, budgetName, budgetLimit);
     }
 
+    /**
+     * Method to test the getBudgetID
+     */
     public void testGetBudgetID(){
-        System.out.println("Starting TestBudgetCategories");
         assertEquals("1", newBudget.getBudgetID());
         assertNotEquals("100",newBudget.getBudgetID());
     }
+
+    /**
+     * Method to test the getBudgetName
+     */
     public void testGetBudgetName(){
         assertEquals("Groceries", newBudget.getBudgetName());
         assertNotEquals("Rent", newBudget.getBudgetName());
     }
+
+    /**
+     * Method to test the getBudgetLimit
+     */
     public void testGetBudgetLimit(){
         assertEquals(100.00,newBudget.getBudgetLimit());
         assertNotEquals(200.00, newBudget.getBudgetLimit());
     }
+
+    /**
+     * Method to test the toString
+     */
     public void testToString(){
         assertEquals("Budget: 1 Groceries "+budgetLimit, newBudget.toString());
         assertNotEquals("Budget: Groceries 1"+budgetLimit, newBudget.toString());
     }
+
+    /**
+     * Method to test the equals
+     */
     public void testEquals(){
         budgetCategories Budget1 = new budgetCategories("1","Groceries", 100.00);
         assertNotNull(Budget1);
@@ -50,12 +78,12 @@ public class TestBudgetCategories extends TestCase
         assertNotNull(Budget3);
         assertFalse(newBudget.equals(Budget3));//only ID is unique, not the budget name or limit
 
+        //won't create an object if passed negative value in budgetLimit
         try {
             budgetCategories Budget4 = new budgetCategories("4", "Phone", -20.00);
             fail("Expected limit will be positive");
         }catch (IllegalArgumentException e){}
 
-        System.out.println("Finished TestBudgetCategories");
     }
 
 }
