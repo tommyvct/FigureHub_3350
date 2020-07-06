7f5168aa9d3a362bfb87a5748511c478da80404e
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2020-07-05 22:52:00 -0500

    AccessTransaction constructor crashed everything?????

--- app/src/main/java/comp3350/pbbs/presentation/addObject/addTransaction.java
+++ app/src/main/java/comp3350/pbbs/presentation/addObject/addTransaction.java
@@ -3,6 +3,7 @@ package comp3350.pbbs.presentation.addObject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import comp3350.pbbs.R;
+import comp3350.pbbs.business.AccessTransaction;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
@@ -32,13 +33,13 @@ public class addTransaction extends AppCompatActivity
         setContentView(R.layout.activity_add_transaction);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Add Transaction");
 
+        AccessTransaction accessTransaction = new AccessTransaction();
         dateText = findViewById(R.id.dateInput);
         dateText.setOnClickListener(v -> dateText.setOnClickListener(v1 ->
         {
              datePickerDialog = new DatePickerDialog(addTransaction.this,
                                                      (view, year1, monthOfYear, dayOfMonth) ->
                                                      {
-                                                         // TODO: here is the date, deal with it.
                                                          dateText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year1);
                                                      }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
              datePickerDialog.show();
@@ -50,7 +51,6 @@ public class addTransaction extends AppCompatActivity
              timePickerDialog = new TimePickerDialog(addTransaction.this,
                                                      (timePicker, hourOfDay, minute) ->
                                                      {
-                                                         // TODO: here is the time, deal with it
                                                          timeText.setText(hourOfDay + " : " + minute);
                                                      }, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), false);
              timePickerDialog.show();
@@ -58,24 +58,50 @@ public class addTransaction extends AppCompatActivity
 
         findViewById(R.id.addTranssubmit).setOnClickListener(view ->
         {
-            if (false)   // validate fields, use methods from business class
-            {
-             // thatEditText.setError()
-            }
-            else
-            {
-             // transaction.addTransaction(string, string, string);
-                Snackbar.make(view, "Transaction Added!", Snackbar.LENGTH_SHORT)
-                        .addCallback(new Snackbar.Callback()
-                        {
-                            @Override
-                            public void onDismissed(Snackbar transientBottomBar, int event)
-                            {
-                                super.onDismissed(transientBottomBar, event);
-                                finish();
-                            }
-                        }).show();
-            }
+//            if (!accessTransaction.isValidDateTime(dateText.getText().toString(), timeText.getText().toString()))   // validate fields, use methods from business class
+//            {
+//                timeText.setError("Invalid time.");
+//                dateText.setError("Invalid date.");
+//                return;
+//            }
+//            if (!accessTransaction.isValidAmount(((EditText) findViewById(R.id.addTransAmount)).getText().toString()))
+//            {
+//                // TODO: it should be able to tell why it didn't went through
+//                ((EditText) findViewById(R.id.addTransAmount)).setError("Invalid amount.");
+//                return;
+//            }
+//            if (!accessTransaction.isValidDescription(((EditText) findViewById(R.id.addTransDescription)).getText().toString()))
+//            {
+//                ((EditText) findViewById(R.id.addTransDescription)).setError("Invalid description.");
+//                return;
+//            }
+//             // transaction.addTransaction(string, string, string);
+//
+//            if  (accessTransaction.addTransaction
+//                (
+//                    ((EditText) findViewById(R.id.addTransDescription)).getText().toString(),
+//                    dateText.getText().toString(),
+//                    timeText.getText().toString(),
+//                    ((EditText) findViewById(R.id.addTransAmount)).getText().toString(),
+//                    null,   // TODO: card selector
+//                    null   // TODO: budgetCategory selector
+//                ))
+//            {
+//                Snackbar.make(view, "Transaction Added!", Snackbar.LENGTH_SHORT)
+//                        .addCallback(new Snackbar.Callback()
+//                        {
+//                            @Override
+//                            public void onDismissed(Snackbar transientBottomBar, int event)
+//                            {
+//                                super.onDismissed(transientBottomBar, event);
+//                                finish();
+//                            }
+//                        }).show();
+//            }
+//            else
+//            {
+                Snackbar.make(view, "Failed to add Transaction.", Snackbar.LENGTH_LONG).show();
+//            }
         });
     }
 
