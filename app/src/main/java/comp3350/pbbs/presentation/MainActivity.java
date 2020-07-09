2c9d992e9cd30fdb98bc0ed7166c2bb18622c417
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2020-07-08 20:39:19 -0500

    lists now able to automatic refresh after coming back from addObject activities

--- app/src/main/java/comp3350/pbbs/presentation/MainActivity.java
+++ app/src/main/java/comp3350/pbbs/presentation/MainActivity.java
@@ -1,30 +1,18 @@
 package comp3350.pbbs.presentation;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import comp3350.pbbs.R;
-import comp3350.pbbs.application.Main;
 import comp3350.pbbs.presentation.addObject.addTransaction;
 import comp3350.pbbs.presentation.addObject.addCard;
 import comp3350.pbbs.presentation.addObject.addBudgetCategory;
-import comp3350.pbbs.presentation.mainActivityFragments.main_budget;
-import comp3350.pbbs.presentation.mainActivityFragments.main_cards;
-import comp3350.pbbs.presentation.mainActivityFragments.main_transactions;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -77,39 +65,10 @@ public class MainActivity extends AppCompatActivity
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        System.out.println("herewref");
         super.onActivityResult(requestCode, resultCode, data);
-        Fragment f = null;
-        switch(requestCode){
-            case 1:
-                //TODO: This is returning null
-                f = this.getSupportFragmentManager().findFragmentById(R.id.);
 
-                System.out.println(f.getClass().getSimpleName());
-                if(f instanceof main_transactions)
-                {
-                    System.out.println("there");
-
-                    main_transactions transactionsFragment = (main_transactions)f;
-                    transactionsFragment.onActivityResult(requestCode, resultCode, data);
-                }
-                break;
-            case 2:
-                f = this.getSupportFragmentManager().findFragmentById(R.id.main_cards);
-                if(f instanceof main_cards)
-                {
-                    main_cards cardsFragment = (main_cards)f;
-                    cardsFragment.onActivityResult(requestCode, resultCode, data);
-                }
-                break;
-            case 3:
-                f = this.getSupportFragmentManager().findFragmentById(R.id.main_budget);
-                if(f instanceof main_budget)
-                {
-                    main_budget cardsFragment = (main_budget)f;
-                    cardsFragment.onActivityResult(requestCode, resultCode, data);
-                }
-                break;
-        }
+        NavigationUI.setupWithNavController(
+            (BottomNavigationView) findViewById(R.id.bottomNavigationView),
+            Navigation.findNavController(this, R.id.fragment));
     }
 }
