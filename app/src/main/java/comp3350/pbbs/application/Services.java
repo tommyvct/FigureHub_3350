116aa92bbc572310fa5832e03075a466e1a1c01d
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2020-07-09 17:24:25 -0500

    Update Services.java

--- app/src/main/java/comp3350/pbbs/application/Services.java
+++ app/src/main/java/comp3350/pbbs/application/Services.java
@@ -1,42 +1,56 @@
 package comp3350.pbbs.application;
 
 import android.os.Build;
+
 import androidx.annotation.RequiresApi;
+
 import comp3350.pbbs.persistence.StubDatabase;
 
+/**
+ * Services
+ * Group4
+ * PBBS
+ *
+ * This class creates and closes database for all other classes to use.
+ */
 public class Services {
 
-    private static StubDatabase dbAccessService = null;
+    private static StubDatabase dbAccessService = null;             //static stub DB variable
 
     /**
+     * This method creates a new initializes a stub database variable and populates data if the dbName is correct.
      *
-     * @param dbName database name
-     * @return a {@code StubDatabase} instance with given name
+     * @param  dbName A string representing the name of the database.
+     * @return StubDatabase for other classes to use
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public static StubDatabase createDataAccess(String dbName){
-        if (dbAccessService == null){
+    public static StubDatabase createDataAccess(String dbName) {
+        if (dbAccessService == null) {
             dbAccessService = new StubDatabase(dbName);
-            if(dbName.equals(Main.dbName))
+            if (dbName.equals(Main.dbName))
                 dbAccessService.populateData();
         }
         return dbAccessService;
     }
 
     /**
+     * This method returns the database which has been created otherwise it exits from the system.
      *
-     * @param dbName database name
-     * @return {@code null} if currently no data access is created, otherwise the current data access
+     * @param  dbName A string representing the name of the database.
+     * @return StubDatabase once it has been created
      */
-    public static StubDatabase getDataAccess(@SuppressWarnings("unused") String dbName){
-        if(dbAccessService == null){
+    public static StubDatabase getDataAccess(String dbName) {
+        if (dbAccessService == null) {
             System.out.println("Connection to data access has not been established.");
             System.exit(1);
         }
         return dbAccessService;
     }
 
-    public static void closeDataAccess(){
+    /**
+     * This method closes the database access.
+     */
+    public static void closeDataAccess() {
         dbAccessService = null;
     }
 }
