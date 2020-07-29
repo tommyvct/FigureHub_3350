8106bf0306364268be22d6066d9af7c74eaf9b84
h.zheng <hao.zheng89@hotmail.com>
2020-07-28 19:53:34 -0500

    TestBankAccount, AccessBankAccount, TestAccessBankAccount 3 classes completed. StubDatabase updated. Card, AccessCard updated. All tests passed.

--- app/src/test/java/comp3350/pbbs/tests/AllTests.java
+++ app/src/test/java/comp3350/pbbs/tests/AllTests.java
@@ -45,8 +45,9 @@ public class AllTests {
         suite.addTestSuite(TestAccessTransactionRetrieve.class);
         suite.addTestSuite(TestAccessTransactionUpdate.class);
         suite.addTestSuite(TestAccessTransactionDelete.class);
-        suite.addTestSuite(TestAccessUser.class);
         suite.addTestSuite(TestAccessBudgetCategory.class);
+        suite.addTestSuite(TestAccessBankAccount.class);
         suite.addTestSuite(TestAccessCard.class);
+        suite.addTestSuite(TestAccessUser.class);
     }
 }
