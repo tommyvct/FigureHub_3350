c0a7f699fff3af6a269b98575f01eef3afcb31f3
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2020-07-30 18:31:10 -0500

    Swap back to stub database

--- app/src/test/java/comp3350/pbbs/tests/persistence/TestDataAccess.java
+++ app/src/test/java/comp3350/pbbs/tests/persistence/TestDataAccess.java
@@ -34,11 +34,11 @@ public class TestDataAccess extends TestCase {
 
     public void setUp() {
         //initially testing will be done on stub database
-//        dataAccess = new StubDatabase("test");
-//        DataAccess.populateData(dataAccess);
+        dataAccess = new StubDatabase("test");
+        DataAccess.populateData(dataAccess);
        // switching to HSQL database can also be done by following these 2 lines:
-        dataAccess = new DataAccessObject(Main.dbName);
-        dataAccess.open(Main.getDBPathName());
+        //dataAccess = new DataAccessObject(Main.dbName);
+        //dataAccess.open(Main.getDBPathName());
         // If you're testing the data access object, the testValidValues will fail, but If you run
         // It individually itll be fine (darn persistent storage)
     }
